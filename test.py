37e426952f0c8904e9ffa60d2bd98645e8c4607a
Cody Winkler <45709553+m0rph-1@users.noreply.github.com>
2020-02-05 20:43:03 -0600

    Update description

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def parse_options():
 
     global parser
     formatter = lambda prog: argparse.HelpFormatter(prog,max_help_position=50)
-    parser = argparse.ArgumentParser(description='Use -sp to bypass SNMP ACL\'s that don\'t specify deny ip any any', formatter_class=formatter)
+    parser = argparse.ArgumentParser(description='Github Bad Code Scraper', formatter_class=formatter)
     parser.add_argument("-q", "--query", type=str, help="Bad code to scrape for", required=True)
     parser.add_argument("-o", "--org", type=str, help="Organization to search bad code in e.g. Microsoft", required=True)
     args = parser.parse_args()
